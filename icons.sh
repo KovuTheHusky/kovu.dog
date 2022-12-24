185a4b3e99ab16711ed96c729c0c2ba994f95b4d
KovuTheHusky <kovuthehusky@gmail.com>
2022-12-24 00:20:26 -0500

    Use pre-installed imagemagick 6 for build

--- icons.sh
+++ icons.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 ICON="images/avatar.webp"
 
-magick -size `identify -ping -format "%wx%h" $ICON` xc:none -fill $ICON -draw "circle %[fx:w/2],%[fx:h/2] 0,%[fx:min(w/2,h/2)]" favicon.webp
+WIDTH=$(identify -ping -format "%w" $ICON)
+HEIGHT=$(identify -ping -format "%h" $ICON)
+HALF=$((WIDTH/2))
 
-magick $ICON -resize 180x180 apple-touch-icon.png
-magick favicon.webp -resize 192x192 android-chrome-192x192.png
-magick favicon.webp -define icon:auto-resize=16,32,48 -compress zip favicon.ico
-magick $ICON -resize 150x150 mstile-150x150.png
-magick $ICON og.png
+convert -size ${WIDTH}x${HEIGHT} xc:none -fill $ICON -draw "circle ${HALF},${HALF} 0,${HALF}" favicon.webp
+
+convert $ICON -resize 180x180 apple-touch-icon.png
+convert favicon.webp -resize 192x192 android-chrome-192x192.png
+convert favicon.webp -define icon:auto-resize=16,32,48 -compress zip favicon.ico
+convert $ICON -resize 150x150 mstile-150x150.png
+convert $ICON og.png
