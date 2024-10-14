d38c3b680183519fe334dc1e37a83ce17b98341f
KovuTheHusky <kovuthehusky@gmail.com>
2024-10-14 19:51:25 -0400

    Update icon script

--- icons.sh
+++ icons.sh
@@ -6,10 +6,10 @@ WIDTH=$(identify -ping -format "%w" $ICON)
 HEIGHT=$(identify -ping -format "%h" $ICON)
 HALF=$((WIDTH/2))
 
-convert -size ${WIDTH}x${HEIGHT} xc:none -fill $ICON -draw "circle ${HALF},${HALF} 0,${HALF}" favicon.webp
+magick -size ${WIDTH}x${HEIGHT} xc:none -fill $ICON -draw "circle ${HALF},${HALF} 0,${HALF}" favicon.webp
 
-convert $ICON -resize 180x180 apple-touch-icon.png
-convert favicon.webp -resize 192x192 android-chrome-192x192.png
-convert favicon.webp -define icon:auto-resize=16,32,48 -compress zip favicon.ico
-convert $ICON -resize 150x150 mstile-150x150.png
-convert $ICON og.png
+magick $ICON -resize 180x180 apple-touch-icon.png
+magick favicon.webp -resize 192x192 android-chrome-192x192.png
+magick favicon.webp -define icon:auto-resize=16,32,48 -compress zip favicon.ico
+magick $ICON -resize 150x150 mstile-150x150.png
+magick $ICON og.png
